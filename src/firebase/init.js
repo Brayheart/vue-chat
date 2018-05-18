7b76f73b75972fd09eb6d9344197905de2ee54d8
Tyler Bray <broncobray77@gmail.com>
2018-05-18 15:38:53 -0700

    welcome page

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBj2W0xrfl1l8OYZcZnM08hDM-QSqOPIdo",
+authDomain: "ninja-chat-5e6b8.firebaseapp.com",
+databaseURL: "https://ninja-chat-5e6b8.firebaseio.com",
+projectId: "ninja-chat-5e6b8",
+storageBucket: "ninja-chat-5e6b8.appspot.com",
+messagingSenderId: "556341106999"
+};
+const firebaseApp = firebase.initializeApp(config);
+// firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebaseApp.firestore()
